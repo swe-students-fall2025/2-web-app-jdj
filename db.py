a2e19cc69824191eef08a6d91bf91991a59110cc
SnazzyBeatle115 <snazzybeatle115@gmail.com>
2025-10-19 10:41:40 -0400

    working db connecion

--- db.py
+++ db.py
@@ -4,15 +4,16 @@ uses pymongo
 includes startup
 """
 
-import pymongo
-from bson.objectid import ObjectId
-import datetime
 import os
+import pymongo
 from dotenv import load_dotenv
+from bson.objectid import ObjectId
 
 
 def dbConnect():
     load_dotenv()
+
+
 
     cxn = pymongo.MongoClient(os.getenv("MONGO_URI"))
     db = cxn[os.getenv("MONGO_DBNAME")]
