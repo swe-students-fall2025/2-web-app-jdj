160d28c5cdc0a32fd8ec8d19ab96e9998b24f2d5
SnazzyBeatle115 <snazzybeatle115@gmail.com>
2025-10-19 15:26:40 -0400

    running app

--- app.py
+++ app.py
@@ -1,30 +1,55 @@
 from flask import Flask, render_template, request, redirect, abort, url_for, make_response
 import flask_login
-from dotenv import load_dotenv
+from dotenv import load_dotenv, dotenv_values
 import pymongo
 from bson.objectid import ObjectId
 import datetime
 import os
+import db as database
 
 load_dotenv()
 
-MONGO_DBNAME = os.getenv('MONGO_DBNAME')
-MONGO_URI = os.getenv('MONGO_URI')
-PORT = int(os.getenv("PORT"))
-SECRET_KEY = os.getenv('SECRET_KEY')
+def create_app():
+    """
+    Create and configure Flash app
+    returns app
+    """
 
-app = Flask(__name__)
-app.secret_key = SECRET_KEY
+    db = database.db_connect()
 
-connection = pymongo.MongoClient(MONGO_URI)
-db = connection[MONGO_DBNAME]
+    
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
-login_manager = flask_login.LoginManager()
-login_manager.login_view = "login" 
-login_manager.init_app(app)
+    app = Flask(__name__)
+    app.secret_key = SECRET_KEY
 
-# routes here
-# need to add login section and home page first
+    config = dotenv_values()
+    app.config.from_mapping(config)
+
+    login_manager = flask_login.LoginManager()
+    login_manager.login_view = "login" 
+    login_manager.init_app(app)
+
+    # routes here
+    # need to add login section and home page first
+
+    @app.route("/")
+    def root():
+        """
+        Route for the root page.
+        Returns:
+            rendered template (str): The rendered HTML template.
+        """
+        response = make_response("hellow world")
+        response.mimetype = "text/plain"
+        return response
+
+
+    return app
+
+app = create_app()
 
 if __name__ == "__main__":
+    PORT = int(os.getenv("FLASK_PORT"))
     app.run(port=PORT, debug=True)
+    print("Flask App running on port", PORT)
